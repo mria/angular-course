ffd91e1fc6d625c1f95a6093e48a011dc9f5e16c
maria.robertsson <maria.robertsson@ericsson.com>
2020-02-05 16:44:16 +0100

    After pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ import { Course } from './model/course';
 export class AppComponent {
 
   courses = COURSES;
+  title = COURSES[0].description;
+  price = 9.9912345;
+  rate = 0.67;
+  course = COURSES[0];
+  startDate = new Date(2000, 0, 1);
 
   onCourseSelected(course: Course) {
     console.log('Card clicked....', course);
