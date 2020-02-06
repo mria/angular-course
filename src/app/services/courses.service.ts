4af215b6b691c202c90db34a1f7bea3deaf536d4
maria.robertsson <maria.robertsson@ericsson.com>
2020-02-06 16:27:35 +0100

    After 22

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Course } from '../model/course';
 
 @Injectable({
@@ -17,5 +17,11 @@ export class CoursesService {
       .set('page', '1')
       .set('pageSize', '10');
     return this.http.get<Course[]>('/api/courses', { params });
- }
+  }
+
+  saveCourse(course: Course) {
+    const headers = new HttpHeaders()
+      .set('X-Auth', 'userId');
+    return this.http.put(`/api/courses/${course.id}`, course, { headers });
+  }
 }
